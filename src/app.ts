6a1f9787b2b466fb42545dab29a205bb2bcf50b9
Leonid Semenov a.k.a. s3rious <canismajor@kaschenka.ru>
2019-01-20 08:18:23 +0500

    Removed chat.ts completely, it's not really had to be in ts module anyway.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,5 @@ import '@babel/polyfill'
 import './css/index.css'
 
 import Video from './components/video'
-// import Chat from './components/chat'
 
 const video = new Video(document.getElementById('video'), document.body)
-// const chat = new Chat('32xHE', window)
