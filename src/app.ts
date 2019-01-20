4b392a0e782e60352a46902615ffa29721dc600b
Leonid Semenov a.k.a. s3rious <canismajor@kaschenka.ru>
2019-01-20 08:13:49 +0500

    Fixed chat css, removed onresize listener. Temporary remove Chat.ts, for some reason it's not working properly.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import '@babel/polyfill'
 import './css/index.css'
 
 import Video from './components/video'
-import Chat from './components/chat'
+// import Chat from './components/chat'
 
 const video = new Video(document.getElementById('video'), document.body)
-const chat = new Chat('32xHE', window)
+// const chat = new Chat('32xHE', window)
--- src/components/chat.ts
+++ src/components/chat.ts
@@ -17,7 +17,7 @@ class Chat {
     this.mount()
   }
 
-  load = () => {
+  load = (): Promise<void> => new Promise((resolve) => {
     const { id, window } = this
     const chats = { encodedChatId: id }
 
@@ -32,19 +32,49 @@ class Chat {
     const xhr = new XMLHttpRequest()
     xhr.withCredentials = true
 
-    // tslint:disable-next-line:no-eval
-    xhr.onload = () => eval(xhr.responseText)
+    xhr.onload = () => {
+      // tslint:disable-next-line:no-eval
+      eval(xhr.responseText)
+      resolve()
+    }
     xhr.onerror = () => console.error('Chatbro loading error')
     const query = btoa(unescape(encodeURIComponent(JSON.stringify(params))))
     xhr.open('GET', `//www.chatbro.com/embed.js?${query}`, false)
     xhr.send()
-  }
+  })
 
   handleResize = debounce(this.load, Chat.debounce)
 
-  mount = () => {
+  mount = async () => {
     this.load()
-    this.window.addEventListener('resize', this.handleResize)
+
+    // TODO: this is a BODGE, around some weied stuff, delete it
+    let root = document.getElementById('#chatbro')
+    if (!root) {
+      root = document.body
+    }
+    const target = root.querySelector('#chatbro .chatbro_messages_empty')
+
+    if (!target) {
+      return
+    }
+
+    const observer = new MutationObserver(function(mutations) {
+      // check for removed target
+      mutations.forEach(function(mutation) {
+        console.log(observer)
+        setTimeout(() => {
+          const elm = document.querySelector('.chatbro_messages_wrapper')
+          const messages = document.querySelectorAll('.chatbro_message')
+          if (messages && messages.length > 1 && elm) {
+            elm.scroll(0, 999999999999999999999999)
+            observer.disconnect()
+          }
+        }, 100)
+      })
+    })
+
+    observer.observe(root, { subtree: true, childList: true })
   }
 }
 
